5dd3b8297d3cf620eff1ceb70a32c74534790d9b
Macallan Savett <jsavett@gmail.com>
2020-08-17 15:10:45 -0700

    attempting to get static folder working

--- CoverLetterGenerator/settings.py
+++ CoverLetterGenerator/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'coverLetters',
+
 ]
 
 
@@ -120,4 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
