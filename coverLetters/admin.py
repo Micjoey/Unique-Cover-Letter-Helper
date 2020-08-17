9eeac1312a65554666e4f3a6c406d7b78925e31b
Macallan Savett <jsavett@gmail.com>
2020-08-17 13:30:18 -0700

    create form to enter info

--- coverLetters/admin.py
+++ coverLetters/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Job
+from .models import Job, CoverLetter
 
 admin.site.register(Job)
+admin.site.register(CoverLetter)
 
--- coverLetters/forms.py
+++ coverLetters/forms.py
@@ -1,8 +1,20 @@
 from django import forms
 from .models import Job
 
-class CoverLetterForm(forms.Form):
-    company = forms.CharField(label="Company Name", empty_value=True)
-    title = forms.CharField(label="Job Title", empty_value=True)
-
+class CoverLetterForm(forms.ModelForm):
+    class Meta:
+        model = Job
+        fields = ['company', 
+            'title', 
+            'link', 
+            'top_skills',
+            'extra_line_one',
+            'extra_line_two',
+            'extra_line_three',
+            'extra_line_four',
+            'extra_line_five',
+            'extra_line_six',
+            'extra_line_seven',
+            'extra_line_eight',
+        ]
         
