121210f0cee573933f9191c79f0af864108b3509
Macallan Savett <jsavett@gmail.com>
2020-08-17 15:15:39 -0700

    add static files in

--- CoverLetterGenerator/settings.py
+++ CoverLetterGenerator/settings.py
@@ -123,6 +123,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static')
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
