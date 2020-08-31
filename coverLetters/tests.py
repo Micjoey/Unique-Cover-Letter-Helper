7266853cb2feb1d298bddb5b6e4874827bd4d92d
Macallan Savett <jsavett@gmail.com>
2020-08-31 14:10:29 -0700

    add a test for cover letter forms

--- coverLetters/tests.py
+++ coverLetters/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from selenium import webdriver
+from .forms import CoverLetterForm
+
+
 
 # class FunctionalTestCase(TestCase):
 #     def setUp(self):
@@ -18,8 +21,32 @@ class UnitTestCaste(TestCase):
 
     def test_home_homepage_template(self):
         response = self.client.get('/')
-        self.assertTemplateUsed(
-            response, 'homepage/homepage.html')
+        self.assertTemplateUsed(response, 'homepage/homepage.html')
+
+    def test_cover_letter_form(self):
+        form = CoverLetterForm(data={
+            'template_choices': 'Template 1',
+            'company':'Test - company1',
+            'city':'Test-  santa barbara',
+            'title':'Test - Jackie',
+            'link':'Test - www.trialone.com',
+            'recruiter':'',
+            'description':'Test - I love test cases',
+            'pre_bullet_point_paragraph_one':'Test - paragraphone',
+            'pre_bullet_point_paragraph_two':'Test - paragraphtwo',
+            'top_skills':'Test - Javascript, Banana',
+            'bullet_point_one':'Test - BP1',
+            'bullet_point_two':'Test - BP2',
+            'bullet_point_three':'Test - BP3',
+            'bullet_point_four':'Test - BP4',
+            'bullet_point_five':'Test - BP5',
+            'bullet_point_six':'Test - BP6',
+            'bullet_point_seven':'Test - BP7',
+            'bullet_point_eight':'Test - BP8',
+            'post_bullet_point_paragraph_one':'Test - paragraphpostone',
+            'post_bullet_point_paragraph_two': 'Test - paragraphposttwo',
+        })
+        self.assertTrue(form.is_valid())
 
     def test_all_jobs_template(self):
         response = self.client.get('/cover-letter-generator/all-jobs')
@@ -29,6 +56,3 @@ class UnitTestCaste(TestCase):
         response = self.client.get('/cover-letter-generator/all-users')
         self.assertTemplateUsed(response, 'users/all-users.html')
 
-    def test_job_detail_template(self):
-        response = self.client.get('/cover-letter-generator/job-detail/1')
-        self.assertTemplateUsed(response, 'job-detail/1')
