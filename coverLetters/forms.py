0864982427cb971099e05d0f7af28172e86a4914
Macallan Savett <jsavett@gmail.com>
2020-09-06 10:04:37 -0700

    move around the job model to make the cover letter form cleaner

--- coverLetters/forms.py
+++ coverLetters/forms.py
@@ -4,37 +4,38 @@ from .models import Job
 from .models import UserDetail
 
 class CoverLetterForm(ModelForm):
-    
     class Meta:
         model = Job
-        fields = [
-            'template_choices',
-            'company', 
-            'job_posting_website',
-            'city',
-            'title', 
-            'link', 
-            'recruiter',
-            'description',
-            'pre_bullet_point_paragraph_one',
-            'pre_bullet_point_paragraph_two',
-            'top_skills',
-            'bullet_point_one',
-            'bullet_point_two',
-            'bullet_point_three',
-            'bullet_point_four',
-            'bullet_point_five',
-            'bullet_point_six',
-            'bullet_point_seven',
-            'bullet_point_eight',
-            'post_bullet_point_paragraph_one',
-            'post_bullet_point_paragraph_two',
-        ]
+        # fields = [
+        #     'template_choices',
+        #     'job_posting_website',
+        #     'company', 
+        #     'city',
+        #     'title', 
+        #     'link', 
+        #     'recruiter',
+        #     'description',
+        #     'pre_bullet_point_paragraph_one',
+        #     'pre_bullet_point_paragraph_two',
+        #     'top_skills',
+        #     'bullet_point_one',
+        #     'bullet_point_two',
+        #     'bullet_point_three',
+        #     'bullet_point_four',
+        #     'bullet_point_five',
+        #     'bullet_point_six',
+        #     'bullet_point_seven',
+        #     'bullet_point_eight',
+        #     'post_bullet_point_paragraph_one',
+        #     'post_bullet_point_paragraph_two',
+        # ]
+        fields = '__all__'
         labels = {'title': 'Title'}
         widgets = {
             # 'bullet_point_one': forms.TextInput,
             'template_choices': forms.RadioSelect(),
         }
+
         formId = forms.CharField(widget=forms.HiddenInput())
 
         
