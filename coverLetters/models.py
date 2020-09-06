b1563eff44092010beeb519a2f48988ebecb25c2
Macallan Savett <jsavett@gmail.com>
2020-09-06 10:04:37 -0700

    move around the job model to make the cover letter form cleaner

--- coverLetters/models.py
+++ coverLetters/models.py
@@ -25,15 +25,26 @@ class UserDetail(models.Model):
         return 'ID ' + str(self.id) + ' - ' + self.first_name + ' ' + self.last_name + ' - date created: ' + str(self.created_date)
 
 class Job(models.Model):
+    model_template_choices = (
+        ('Template 1', 'cover-letter-1'),
+        ('Template 2', 'cover-letter-2'),
+        ('Template 3', 'cover-letter-3'),
+        ('Template 4', 'cover-letter-4'),
+        ('Template 5', 'cover-letter-5')
+        )
+    template_choices = models.CharField(choices=model_template_choices, default='Template 1', max_length=20)
+    choice_of_user = models.ForeignKey('UserDetail', on_delete=models.CASCADE, blank=True, null=True)
+    job_posting_website = models.CharField(max_length=200, blank=True)
     company = models.CharField(max_length = 200, blank=True)
     title = models.CharField(max_length = 200)
-    job_posting_website = models.CharField(max_length=200)
     link = models.CharField(max_length = 200, blank=True)
     recruiter = models.CharField(max_length = 200,  blank=True)
     city = models.CharField(max_length = 200,  blank=True)
     top_skills = models.TextField(
          blank=True, default='Dynamic and accomplished Software Engineer with experience and expertise in')
     description = models.TextField( blank=True)
+    pre_bullet_point_paragraph_one = models.TextField( blank=True)
+    pre_bullet_point_paragraph_two = models.TextField( blank=True)
     bullet_point_one = models.TextField( blank=True)
     bullet_point_two = models.TextField( blank=True)
     bullet_point_three = models.TextField( blank=True)
@@ -42,19 +53,8 @@ class Job(models.Model):
     bullet_point_six = models.TextField( blank=True)
     bullet_point_seven = models.TextField( blank=True)
     bullet_point_eight = models.TextField( blank=True)
-    pre_bullet_point_paragraph_one = models.TextField( blank=True)
-    pre_bullet_point_paragraph_two = models.TextField( blank=True)
     post_bullet_point_paragraph_one = models.TextField( blank=True)
     post_bullet_point_paragraph_two = models.TextField( blank=True)
-    model_template_choices = (
-        ('Template 1', 'cover-letter-1'),
-        ('Template 2', 'cover-letter-2'),
-        ('Template 3', 'cover-letter-3'),
-        ('Template 4', 'cover-letter-4'),
-        ('Template 5', 'cover-letter-5')
-        )
-    choice_of_user = models.ForeignKey('UserDetail', on_delete=models.CASCADE, blank=True, null=True)
-    template_choices = models.CharField(choices=model_template_choices, default='Template 1', max_length=20)
     modified_date = models.DateField(auto_now=True)
     created_date = models.DateField(auto_now_add=True)
 
