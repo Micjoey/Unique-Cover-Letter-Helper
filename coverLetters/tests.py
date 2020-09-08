e041e986246c31538bea168c126eaa7fff74a3f8
Macallan Savett <jsavett@gmail.com>
2020-09-08 15:17:06 -0700

    Test all form types and users

--- coverLetters/tests.py
+++ coverLetters/tests.py
@@ -19,67 +19,67 @@ class FunctionalTestCase(TestCase):
     def home_page_form_text_link(self):
         return 'http://localhost:3000'
 
-    # def test_all_links_on_homepage(self):
-    #     self.browser.get(self.home_page_form_text_link())
-    #     allATags = self.browser.find_elements_by_tag_name('a')
-    #     i = 0
-    #     while i < len(allATags):
-    #         self.browser.find_elements_by_tag_name('a')[i].click()
-    #         self.browser.back()
-    #         i+=1
+    def test_all_links_on_homepage(self):
+        self.browser.get(self.home_page_form_text_link())
+        allATags = self.browser.find_elements_by_tag_name('a')
+        i = 0
+        while i < len(allATags):
+            self.browser.find_elements_by_tag_name('a')[i].click()
+            self.browser.back()
+            i+=1
 
         
-    # def test_there_is_homepage(self):
-    #     self.browser.get(
-    #         self.home_page_form_text_link())
-    #     self.assertIn('Job Application Process', self.browser.page_source)
+    def test_there_is_homepage(self):
+        self.browser.get(
+            self.home_page_form_text_link())
+        self.assertIn('Job Application Process', self.browser.page_source)
         
 
-    # def test_cover_letter_form_button(self):
-    #     self.browser.get(self.home_page_form_text_link())
-    #     self.browser.find_element_by_id(
-    #         'homepage-cover-letter-template-button').click()
+    def test_cover_letter_form_button(self):
+        self.browser.get(self.home_page_form_text_link())
+        self.browser.find_element_by_id(
+            'homepage-cover-letter-template-button').click()
 
-    # def test_user_creation_form_button(self):
-    #     self.browser.get(self.home_page_form_text_link())
-    #     self.browser.find_element_by_id('homepage-user-template-button').click()
+    def test_user_creation_form_button(self):
+        self.browser.get(self.home_page_form_text_link())
+        self.browser.find_element_by_id('homepage-user-template-button').click()
     
-    # def test_all_users_button(self):
-    #     self.browser.get(self.home_page_form_text_link())
-    #     self.browser.find_element_by_id('homepage-all-users-button').click()
+    def test_all_users_button(self):
+        self.browser.get(self.home_page_form_text_link())
+        self.browser.find_element_by_id('homepage-all-users-button').click()
     
-    # def test_all_jobs_button(self):
-    #     self.browser.get(self.home_page_form_text_link())
-    #     self.browser.find_element_by_id('homepage-all-jobs-button').click()
+    def test_all_jobs_button(self):
+        self.browser.get(self.home_page_form_text_link())
+        self.browser.find_element_by_id('homepage-all-jobs-button').click()
     
-    # def test_admin_button(self):
-    #     self.browser.get(self.home_page_form_text_link())
-    #     self.browser.find_element_by_id('homepage-admin-button').click()
+    def test_admin_button(self):
+        self.browser.get(self.home_page_form_text_link())
+        self.browser.find_element_by_id('homepage-admin-button').click()
     
-    # def test_cover_letter_back_button(self):
-    #     self.browser.get(
-    #         self.main_form_text_link())
-    #     self.browser.find_element_by_link_text('Back').click()
+    def test_cover_letter_back_button(self):
+        self.browser.get(
+            self.main_form_text_link())
+        self.browser.find_element_by_link_text('Back').click()
     
-    # def test_triplebyte_back_button(self):
-    #     self.browser.get(
-    #         'http://localhost:3000/cover-letter-generator/forms/triplebyte-form')
-    #     self.browser.find_element_by_link_text('Back').click()
+    def test_triplebyte_back_button(self):
+        self.browser.get(
+            'http://localhost:3000/cover-letter-generator/forms/triplebyte-form')
+        self.browser.find_element_by_link_text('Back').click()
     
-    # def test_user_form_back_button(self):
-    #     self.browser.get(
-    #         'http://localhost:3000/cover-letter-generator/user-form')
-    #     self.browser.find_element_by_link_text('Back').click()
+    def test_user_form_back_button(self):
+        self.browser.get(
+            'http://localhost:3000/cover-letter-generator/user-form')
+        self.browser.find_element_by_link_text('Back').click()
     
-    # def test_all_users_back_button(self):
-    #     self.browser.get(
-    #         'http://localhost:3000/cover-letter-generator/all-users')
-    #     self.browser.find_element_by_link_text('Back').click()
+    def test_all_users_back_button(self):
+        self.browser.get(
+            'http://localhost:3000/cover-letter-generator/all-users')
+        self.browser.find_element_by_link_text('Back').click()
     
-    # def test_all_jobs_back_button(self):
-    #     self.browser.get(
-    #         'http://localhost:3000/cover-letter-generator/all-jobs')
-    #     self.browser.find_element_by_link_text('Back').click()
+    def test_all_jobs_back_button(self):
+        self.browser.get(
+            'http://localhost:3000/cover-letter-generator/all-jobs')
+        self.browser.find_element_by_link_text('Back').click()
 
     def test_filling_out_cover_letter_form(self):
         self.browser.get(self.main_form_text_link())
@@ -95,14 +95,19 @@ class FunctionalTestCase(TestCase):
                 for tag in input_tags:
                     tag_id = tag.get_attribute('id')
                     if tag_id and not tag_id == 'submit-button':
-                        print(tag_id)
                         self.browser.find_element_by_id(tag_id).send_keys('hello')
                 self.browser.find_element_by_id("submit-button").click()
                 self.browser.get(
                     self.main_form_text_link())
                 staleness = WebDriverWait(self.browser, 10).until(
-                    EC.staleness_of(all_template_choices.options[i]))
-                print(staleness)
+                    EC.staleness_of(input_tags[2]))
+                if staleness:
+                    all_template_choices = Select(
+                        self.browser.find_element_by_name('template_choices'))
+                    all_template_users = Select(
+                        self.browser.find_element_by_name('choice_of_user'))
+                    input_tags = self.browser.find_elements_by_tag_name(
+                        'input')
                 x += 1
             i += 1
 
