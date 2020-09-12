b8ca4aadf0ea59db1116430da3ed4575b06c2983
Macallan Savett <jsavett@gmail.com>
2020-09-12 13:49:25 -0700

    accept incoming changes

--- coverLetters/views.py
+++ coverLetters/views.py
@@ -100,20 +100,6 @@ def user_form(request):
         return render(request, 'users/user-form.html', {'userForm': form})
 
 
-<<<<<<< HEAD
-def submit_to_interview_db(interviewDBInfo):
-    http = urllib3.PoolManager()
-    data = {
-        'css-1hwfws3': interviewDBInfo['company'],
-        'root_applications_0_jobTitle': interviewDBInfo['job_title'],
-        'css-151xaom-placeholder': interviewDBInfo['source']
-    }
-    r = http.request('GET',
-                     'https://www.interview-db.com/',
-                     )
-
-    print(r.data)
-=======
 # def submit_to_interview_db(interviewDBInfo):
 #     http = urllib3.PoolManager()
 #     data = {
@@ -126,5 +112,4 @@ def submit_to_interview_db(interviewDBInfo):
 #                      )
 
 #     print(r.data)
->>>>>>> 9ae0feb... edit tests.py
     
