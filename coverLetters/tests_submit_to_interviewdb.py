8cca35fa6d9a7fb72bc1c324a4da4d32a86f9c43
Macallan Savett <jsavett@gmail.com>
2020-09-14 10:01:58 -0700

    remove print

--- coverLetters/tests_submit_to_interviewdb.py
+++ coverLetters/tests_submit_to_interviewdb.py
@@ -27,7 +27,6 @@ class FunctionalSubmitToInterviewDB(TestCase):
         allJobs = self.browser.find_elements_by_tag_name('a')
         i = 2
         while i < 110:
-            print("current index -", i)
             currentJob = allJobs[i]
             currentJob.click()
             time.sleep(2)
@@ -53,7 +52,6 @@ class FunctionalSubmitToInterviewDB(TestCase):
                 self.browser.find_element_by_class_name(
                     'btn-block').click()
                 wait.until(EC.url_changes(self.browser))
-                print('signed in')
                 self.browser.get('https://www.interview-db.com/profile')
                 reportLink = self.browser.find_element_by_css_selector(
                     '#root > section > div > nav > a:nth-child(1)')
@@ -63,39 +61,24 @@ class FunctionalSubmitToInterviewDB(TestCase):
             self.browser.get('https://www.interview-db.com/profile')
             self.browser.find_element_by_xpath(
                 '//*[@id="root"]/section/div/main/nav/nav/button[3]').click()
-            print('go back to interview-db-job-search')
             wait.until(
                 EC.element_to_be_clickable((By.TAG_NAME, 'li')))
-            print('click on all activity')
             self.browser.find_element_by_tag_name('input').clear()
             self.browser.find_element_by_tag_name('input').send_keys('365')
-            print('enter search day')
             time.sleep(10)
             fullTitleIsPresent = self.browser.page_source.find(
                 jobDetails) != -1
             halfTitleIsPresent = halfJobDetails in self.browser.page_source
-            print(fullTitleIsPresent," -", jobDetails)
             if not fullTitleIsPresent or not halfTitleIsPresent:
-                print('in NOT present if clause')
                 self.browser.find_element_by_xpath('//*[@id="root"]/section/div/nav/a[1]').click()
-                print('click on report')
                 wait.until(EC.invisibility_of_element((By.CLASS_NAME, 'sc-lcpuFF eOXROa')))
-                print('clear clickable wait for li')
-                # time.sleep(10)
-                # self.browser.find_element_by_xpath('/html/body/main/section/div/div/div/div/ul/li[2]').click()
-                # print('click on job search updates')
                 if wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'btn-add'))):
-                    print("clear btn wait")
                     self.browser.find_elements_by_class_name('btn-add')[5].click()
-                    print('clicked button')
-                    # find the Job Title input field
                     title = self.browser.find_element_by_id(
                         'root_applications_0_jobTitle')
                     title.click()
                     title.send_keys(
                         jobTitle)
-                    #
-                    # find the company field and create it or select
                     actions = ActionChains(self.browser)
                     companyButton = self.browser.find_elements_by_class_name(
                         'css-1hwfws3')[0]
@@ -125,11 +108,9 @@ class FunctionalSubmitToInterviewDB(TestCase):
                     actions.reset_actions()
                     #
                 self.browser.find_elements_by_tag_name('button')[9].click()
-                print('submit job')
                 wait.until(EC.url_matches(
                     'https://www.interview-db.com/profile/job-search'))
                 # time.sleep(10)
-            print('break from if clause')
             self.browser.get(
                 'http://localhost:3000/cover-letter-generator/all-jobs/')
             allJobs = self.browser.find_elements_by_tag_name('a')
