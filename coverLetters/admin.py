77da34a87731553d6f670fe5df424d28943c8def
Macallan Savett <jsavett@gmail.com>
2020-11-20 11:47:02 -0800

    Move assets file

--- coverLetters/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Job, UserDetail
-
-admin.site.register(Job)
-admin.site.register(UserDetail)
-
-
-class JobsAdmin(admin.ModelAdmin):
-    readonly_fields = ('id',)
