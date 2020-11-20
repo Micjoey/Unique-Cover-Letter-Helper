ce22f96c3298754938aef78fb3cafb3a20b53552
Macallan Savett <jsavett@gmail.com>
2020-11-19 20:38:20 -0800

    add more to the frontend

---
+++ frontend/gui/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import 'antd/dist/antd.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/gui/src/containers/layout.js
@@ -0,0 +1,8 @@
+import react from react
+import {Layout, Menu, Breadcrump} from 'antd'
+
+const CustomLayout = (props) => {
+    return (
+        <h1>Hello World - Layout</h1>
+    )
+}
