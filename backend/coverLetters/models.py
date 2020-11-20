b52b1aeee1afac67f3b886f961fc1b18ef427975
Macallan Savett <jsavett@gmail.com>
2020-11-20 11:44:53 -0800

    move files to the backend folder

---
+++ backend/coverLetters/models.py
@@ -0,0 +1,102 @@
+from django.db import models
+from django.db.models import Q, UniqueConstraint
+from phone_field import PhoneField
+from multiselectfield import MultiSelectField
+import datetime
+from datetime import datetime
+
+__all__ = ['CheckConstraint', 'UniqueConstraint']
+class UserDetail(models.Model):
+    first_name = models.CharField(max_length=200, blank=False)
+    middle_name = models.CharField(max_length=200,  blank=True)
+    last_name = models.CharField(max_length=200, blank=False)
+    preferred_name = models.CharField(max_length=200, blank=True)
+    phone_number = PhoneField(blank=True)
+    email = models.EmailField(blank=True, null=True)
+    linkedin = models.URLField(blank=True, null=True)
+    github = models.URLField(blank=True, null=True)
+    portfolio_website = models.URLField(blank=True, null=True)
+    street_address = models.CharField(blank=True, max_length=200)
+    city_address = models.CharField(blank=True, max_length=200,)
+    state_address = models.CharField(blank=True, max_length=200,)
+    zip_code = models.CharField(blank=True, max_length=200,)
+    modified_date = models.DateField(auto_now=True, blank=True)
+    created_date = models.DateField(auto_now_add=True, blank=True)
+    
+
+    class Meta:
+        ordering = ['-modified_date']
+
+
+    def __str__(self):
+        return 'ID ' + str(self.id) + ' - ' + self.first_name + ' ' + self.last_name + ' - date created: ' + str(self.created_date)
+
+class Job(models.Model):
+    job_template_choices = (
+        ('Standard Job Template', 'Standard Job Template'),
+        ('Triplebyte (message-version)', 'Triplebyte (message-version)'),
+        ('non-technical-cover-letter', 'Non-technical Cover Letter'),
+        ('cover-letter-4', 'Template 4'),
+        ('cover-letter-5', 'Template 5')
+    )
+
+
+    job_stages = (
+        ('Initial', 'Initial'),
+        ('Active', 'Active'),
+        ('Accepted', 'Accepted'),
+        ('Rejected', 'Rejected'),
+        ('No Response', 'No Response')
+    )
+
+    form_creation_date = models.CharField(
+        max_length=100, blank=True, default=datetime.now().strftime('%B %dth, %Y'))
+    template_choices = models.CharField(
+        choices=job_template_choices, default='Standard Job Template', max_length=100, null=False, blank='False')
+    job_stage = models.CharField(
+        choices=job_stages, default='Rejected', max_length=100, null=False, blank='False')
+    choice_of_user = models.ForeignKey(
+        'UserDetail', on_delete=models.CASCADE, blank=True, null=True)
+    job_posting_website = models.CharField("Job Posting Website", max_length=200,)
+    position_title = models.CharField("Position Title",max_length = 200)
+    company = models.CharField(max_length = 200, blank=True)
+    city = models.CharField(max_length = 200,  blank=True)
+    link = models.CharField(max_length = 200)
+    recruiter = models.CharField(max_length = 200,  blank=True)
+    description = models.TextField( blank=True)
+    pre_bullet_point_paragraph_one = models.TextField( blank=True)
+    pre_bullet_point_paragraph_two = models.TextField( blank=True)
+    top_skills = models.TextField(
+         blank=True, default='Dynamic and accomplished Software Engineer with experience and expertise in')
+    bullet_point_one = models.TextField( blank=True)
+    bullet_point_two = models.TextField( blank=True)
+    bullet_point_three = models.TextField( blank=True)
+    bullet_point_four = models.TextField( blank=True)
+    bullet_point_five = models.TextField( blank=True)
+    bullet_point_six = models.TextField( blank=True)
+    bullet_point_seven = models.TextField( blank=True)
+    bullet_point_eight = models.TextField( blank=True)
+    post_bullet_point_paragraph_one = models.TextField( blank=True)
+    post_bullet_point_paragraph_two = models.TextField( blank=True)
+    modified_date = models.DateField(auto_now=True)
+    created_date = models.DateField(auto_now_add=True)
+    
+
+    class Meta:
+        unique_together = ('link', 'template_choices', 'position_title')
+        ordering = ['-created_date', '-modified_date', '-position_title']
+        get_latest_by = ['-created_date', 'modified_date']
+
+    def __str__(self):
+        return self.company + ' ' + self.position_title + ' - Last Modified: ' + str(self.modified_date) + ' - ' + self.template_choices
+
+    def save(self, *args, **kwargs):
+        if not self.description:
+            self.description = 'N/A'
+        if not self.job_posting_website:
+            self.job_posting_website = self.company
+        super().save(*args, **kwargs)
+
+
+
+    
---
+++ backend/coverLetters/views.py
@@ -0,0 +1,165 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.forms.models import model_to_dict
+from django.core import serializers
+from django.urls import path, include
+from django.http import HttpResponseForbidden
+from .models import Job, UserDetail
+from datetime import datetime, timedelta
+from.forms import CoverLetterForm, UserDetailForm, TripleByteForm
+import urllib3
+import json
+
+
+# Helper Functions
+
+def check_template_choice(request, template_choice, last_user, cleaned_filled_form):
+    if "Standard Job Template" in template_choice:
+        return render(request, 'coverLetters/cover-letter.html', {'job': cleaned_filled_form, 'last_user': last_user, 'template_choice': template_choice})
+    elif "Triplebyte (message-version)" in template_choice:
+        return render(request, 'coverLetters/triplebyte-cover-letter.html', {'job': cleaned_filled_form, 'last_user': last_user, 'template_choice': template_choice})
+    elif "non-technical-cover-letter" in template_choice:
+        return render(request, 'coverLetters/non-technical-cover-letter.html', {'job': cleaned_filled_form, 'last_user': last_user, 'template_choice': template_choice})
+    elif "4" in template_choice:
+        return render(request, 'coverLetters/cover-letter-four.html', {'job': cleaned_filled_form, 'last_user': last_user, 'template_choice': template_choice})
+    elif "5" in template_choice:
+        return render(request, 'coverLetters/cover-letter-five.html', {'job': cleaned_filled_form, 'last_user': last_user, 'template_choice': template_choice})
+
+
+def date_finder(end_date=0):
+    start_date = datetime.now()
+    if end_date != 0:
+        return (start_date - timedelta(days=end_date)).strftime('%Y-%m-%d')
+    else:
+        return start_date.strftime('%Y-%m-%d')
+    
+
+# <--------  ------->
+
+def homepage(request):
+    return render(request, 'homepage/homepage.html')
+
+def all_jobs(request):
+    jobs = Job.objects.order_by('-id')
+    today = date_finder()
+    week_ago = date_finder(7)
+    two_months = date_finder(30)
+    filtered_jobs_today = jobs.filter(
+        created_date=today)
+    filtered_jobs_week = jobs.filter(
+        created_date__range=[week_ago, today])
+    filtered_jobs = [i for i in jobs if i not in filtered_jobs_week]
+    filtered_jobs_previous = jobs.exclude(
+        created_date__range=[week_ago, today])
+    old_jobs = jobs.exclude(
+        created_date__range=[two_months, today])
+    active_jobs = jobs.filter(job_stage="Active")
+
+    for job in old_jobs:
+        if "Rejected" in job.job_stage:
+            continue
+
+        if "Initial" in job.job_stage:
+            job.job_stage = "No Response"
+            job.save()
+
+    return render(request, 'jobs/all-jobs.html', {
+        'jobs': jobs,
+        'filtered_jobs_today': filtered_jobs_today,
+        'filtered_jobs_week': filtered_jobs_week,
+        'filtered_jobs': filtered_jobs,
+        'filtered_jobs_previous': filtered_jobs_previous,
+        'active_jobs': active_jobs,
+    })
+
+def active_jobs(request):
+    jobs = Job.objects.order_by('-id')
+    active_jobs = jobs.filter(job_stage="Active")
+    return render(request, 'jobs/active-jobs.html', {
+        'active_jobs': active_jobs,
+    })
+
+def all_users(request):
+    users = UserDetail.objects.all
+    return render(request, 'users/all-users.html', {
+        'users': users
+    })
+
+def job_detail(request, job_id):
+    job_detail = get_object_or_404(Job, pk=job_id)
+    user_id = int(str(job_detail.choice_of_user).split(" ")[1])
+    user_detail = UserDetail.objects.get(pk=user_id)
+    object = model_to_dict(Job.objects.get(pk=job_id))
+    return render(request, 'jobs/job-detail.html', {'job': job_detail, 'job_object': object, 'user_detail': user_detail})
+
+def delete_job_detail(request, job_id):
+    job_detail = get_object_or_404(Job, pk=job_id)
+    job_detail.delete()
+    return redirect('all-jobs')
+    
+def delete_user_detail(request, user_id):
+    user_detail = get_object_or_404(UserDetail, pk=user_id)
+    user_detail.delete()
+    return redirect('all-users')
+
+def user_detail(request, user_id):
+    user_detail = get_object_or_404(UserDetail, pk=user_id)
+    object = model_to_dict(UserDetail.objects.get(pk=user_id))
+    object_keys = list(object.keys())
+    return render(request, 'users/user-detail.html', {'user': user_detail, 'object_keys': object_keys, 'object': object})
+
+def cover_letter_form(request):
+    new_form = CoverLetterForm()
+    return render(request, 'coverLetters/form-template.html', {'coverLetterForm': new_form})
+
+def triplebyte_message_form(request):
+    new_form=TripleByteForm()
+    return render(request, 'coverLetters/form-template.html', {'coverLetterForm': new_form})
+
+def cover_letter(request):
+    last_user = UserDetail.objects.last
+    if request.method == 'POST':
+        filled_form = CoverLetterForm(request.POST)
+        if filled_form.is_valid():
+            filled_form.save()
+            cleaned_filled_form = filled_form.cleaned_data #turns the form into a dict (object)
+            last_user = cleaned_filled_form['choice_of_user']
+            template_choice = cleaned_filled_form['template_choices']
+            # Checks to see what template to render for the cover letter -->
+            return check_template_choice(request, template_choice,
+                                  last_user, cleaned_filled_form)
+            # <--------  ------->
+    else:
+        form = CoverLetterForm()
+        return render(request, 'coverLetters/form-template.html', {'coverLetterForm': form})
+    print(filled_form.errors.as_json(escape_html=False))
+    return HttpResponseForbidden("Duplicate Data - See Terminal Log")
+
+def edit_job_form(request, job_id):
+    job_detail = get_object_or_404(Job, pk=job_id)
+    form = CoverLetterForm(instance=job_detail)
+    if request.method == 'POST':
+        filled_form = CoverLetterForm(request.POST, instance = job_detail)
+        if filled_form.is_valid():
+            filled_form.save()
+            form = filled_form
+            job_detail = get_object_or_404(Job, pk=job_id)
+            user_id = int(str(job_detail.choice_of_user).split(" ")[1])
+            user_detail = UserDetail.objects.get(pk=user_id)
+            object = model_to_dict(Job.objects.get(pk=job_id))
+            return render(request, 'jobs/job-detail.html', {'job': job_detail, 'job_object': object, 'user_detail': user_detail})
+    return render(request, 'coverLetters/edit-template-form.html', {'coverLetterForm': form, 'job': job_detail,})
+
+def user_form(request):
+    if request.method == 'POST':
+        user_filled_form = UserDetailForm(request.POST)
+        if user_filled_form.is_valid():
+            user_filled_form.save()
+            users = UserDetail.objects.all
+            return render(request, 'users/all-users.html', {'users': users})
+    else:
+        form = UserDetailForm()
+        return render(request, 'users/user-form.html', {'userForm': form})
+
+
+
+
