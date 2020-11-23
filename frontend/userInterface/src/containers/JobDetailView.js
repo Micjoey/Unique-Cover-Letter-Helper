0b8b2fd9ff1bb643e7835341417ea5f037f281d8
Macallan Savett <jsavett@gmail.com>
2020-11-22 17:06:58 -0800

    get forms to update, delete and put properly

--- frontend/userInterface/src/containers/JobDetailView.js
+++ frontend/userInterface/src/containers/JobDetailView.js
@@ -3,11 +3,13 @@ import JobDetail from '../components/JobDetail'
 import {JobForm} from '../components/JobForm'
 import axios from 'axios'
 import { useParams } from 'react-router'
+import { useForm } from 'react-hook-form'
 
 
 const JobDetailView = (id) => {
     const [job, setjob] = useState([])
     const [loaded, setLoaded] = useState({ isLoaded: false })
+    const { handleSubmit } = useForm()
     const paramsJobId = useParams().jobID
     useEffect(() => {
         axios.get(`http://127.0.0.1:3000/api/jobs/${paramsJobId}`)
@@ -18,14 +20,26 @@ const JobDetailView = (id) => {
             })
     }, [])
 
+    const onSubmit = () => {
+        axios.delete(`http://127.0.0.1:3000/api/jobs/${paramsJobId}/`)
+            .then(() => window.location.href = 'http://127.0.0.1:3001/api/jobs/')
+            .catch(error => console.log(error))
+    }
+
     if (loaded.isLoaded) {
         return (
             <div>
                 <div>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <button type="danger" type="submit">Delete</button>
+                    </form>
+                </div>
+                <div>
                     <JobDetail jobDetail={job} />
                 </div>
                 <h2>Update Job:</h2>
-                <JobForm requestType="patch" jobID={paramsJobId} buttonTxt="Update"/>
+                <JobForm requestType="put" jobID={paramsJobId} buttonTxt="Update"/>
+                
             </div>
         )
     } else {
