a2f1af1ec093ceb0f84be3754131d39dba691b81
Macallan Savett <jsavett@gmail.com>
2020-11-24 11:46:34 -0800

    add coverletter route

--- frontend/userInterface/src/routes.js
+++ frontend/userInterface/src/routes.js
@@ -3,10 +3,13 @@ import { Route, Switch } from 'react-router-dom'
 
 import JobListView from './containers/job_container/JobListView'
 import JobDetailView from './containers/job_container/JobDetailView'
+import CoverLetterView from './containers/forms/CoverLetterView'
+
 
 const BaseRouter = () => (
     <div>
         <Switch>
+            <Route exact path='/job/form' component={CoverLetterView}/>
             <Route exact path='/:jobID' component={JobDetailView}/>
             <Route path='/' component={JobListView}/>
         </Switch>
