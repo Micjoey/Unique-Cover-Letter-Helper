56bb52f7417b013f09a848dd562fb930b0e915f6
Macallan Savett <jsavett@gmail.com>
2020-11-24 15:38:18 -0800

    finished pagination

--- frontend/userInterface/src/components/pagination/Pagination.js
+++ frontend/userInterface/src/components/pagination/Pagination.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
 
 
 export default function Pagination(props) {
-    const { pageIndex, total, perPage, onNext, onPrevious } = props;
+    const { pageIndex, total, perPage, onNext, onPrevious, setAllJobs, setNext, setOnPrevious, setPageIndex } = props;
     const lastPage = Math.ceil(total / perPage);
-    const previous = pageIndex > 1 ? (<button onClick={onPrevious}>Previous</button>) : null;
-    const next = pageIndex < lastPage ? (<button onClick={onNext}>Next</button>) : null;
+    const previous = pageIndex > 1 ? (<button onClick={() => previousPage()}>Previous</button>) : null;
+    const next = pageIndex < lastPage ? (<button onClick={() => nextPage()}>Next</button>) : null;
+    
+    const nextPage = () => {
+        axios.get(onNext)
+            .then(res => {
+                setAllJobs(res.data.results)
+                setNext(res.data.next)
+                setOnPrevious(res.data.previous)
+                setPageIndex(pageIndex+1)
+            })
+    }
+
+    const previousPage = () => {
+        axios.get(onPrevious)
+            .then(res => {
+                setAllJobs(res.data.results)
+                setNext(res.data.next)
+                setOnPrevious(res.data.previous)
+                setPageIndex(pageIndex - 1)
+            })
+    }
+
     return (
         <div className="Pagination">
             <div className="Pagination-actions">
--- frontend/userInterface/src/containers/job_container/JobListView.js
+++ frontend/userInterface/src/containers/job_container/JobListView.js
@@ -10,17 +10,19 @@ const JobList = () => {
     const [jobProps, setJobProps] = useState([])
     const [allJobs, setAllJobs] = useState([])
     const [next, setNext] = useState([])
+    const [onPrevious, setOnPrevious] = useState([])
     const [loaded, setLoaded] = useState({isLoaded: false})
     const [pageIndex, setPageIndex] = useState(1)
     const [count, setCount] = useState(0)
 
     useEffect(() => {
-        axios.get('http://127.0.0.1:3000/api/jobs/')
+        axios.get(`http://127.0.0.1:3000/api/jobs/`)
         .then( res => {
             setAllJobs(res.data.results)
             setJobProps(res.data)
             setNext(res.data.next)
             setCount(res.data.count)
+            setOnPrevious(res.data.previous)
         })
         .then(() => {
             setLoaded({isLoaded: true})
@@ -28,13 +30,25 @@ const JobList = () => {
     }, [])
 
     if (loaded.isLoaded) {
-        console.log(next, pageIndex, count)
         return (
             <div>
                 <h1>All Jobs:</h1>
                 <div>
                     <Jobs jobs={allJobs} jobProps={jobProps}/>
                 </div>
+                <footer>
+                    <Pagination 
+                        pageIndex={pageIndex} 
+                        total={count} 
+                        perPage={9} 
+                        onNext={next}
+                        onPrevious={onPrevious}
+                        setAllJobs={setAllJobs}
+                        setNext = {setNext}
+                        setOnPrevious={setOnPrevious}
+                        setPageIndex={setPageIndex}
+                    />
+                </footer>
             </div>
         )
     } else {
