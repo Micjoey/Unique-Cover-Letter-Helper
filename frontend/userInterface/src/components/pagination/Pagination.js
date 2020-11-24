975115c57e7d3d7a8291737c0032042845ff7896
Macallan Savett <jsavett@gmail.com>
2020-11-24 15:42:52 -0800

    add home button

--- frontend/userInterface/src/components/pagination/Pagination.js
+++ frontend/userInterface/src/components/pagination/Pagination.js
@@ -7,10 +7,12 @@ export default function Pagination(props) {
     const lastPage = Math.ceil(total / perPage);
     const previous = pageIndex > 1 ? (<button onClick={() => previousPage()}>Previous</button>) : null;
     const next = pageIndex < lastPage ? (<button onClick={() => nextPage()}>Next</button>) : null;
-    
+    const home = pageIndex > 1 ? (<button onClick={() => goHome()}>Home</button>) : null;
+
     const nextPage = () => {
         axios.get(onNext)
             .then(res => {
+                console.log(res)
                 setAllJobs(res.data.results)
                 setNext(res.data.next)
                 setOnPrevious(res.data.previous)
@@ -28,10 +30,21 @@ export default function Pagination(props) {
             })
     }
 
+    const goHome = () => {
+        axios.get(`http://127.0.0.1:3000/api/jobs/`)
+            .then(res => {
+                setAllJobs(res.data.results)
+                setNext(res.data.next)
+                setOnPrevious(res.data.previous)
+                setPageIndex(1)
+            })
+    }
+
     return (
         <div className="Pagination">
             <div className="Pagination-actions">
                 {previous}
+                {home}
                 {next}
             </div>
             <div className="Pagination-stats">
