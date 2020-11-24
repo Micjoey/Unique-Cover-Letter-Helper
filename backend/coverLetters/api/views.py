7b69d3ba4fa0167541233d435deed6f6b7d7a167
Macallan Savett <jsavett@gmail.com>
2020-11-24 14:45:07 -0800

    setting up pagination

--- backend/coverLetters/api/views.py
+++ backend/coverLetters/api/views.py
@@ -1,13 +1,16 @@
 from rest_framework import viewsets
 from coverLetters.models import Job, UserDetail
 from .serializers import JobSerializer
-# class JobsPagination(LimitOffsetPagination):
-#     default_limit = 50
-#     max_limit = 100
+from rest_framework.pagination import PageNumberPagination
+
+
+class JobsPagination(PageNumberPagination):
+    page_size = 10
 
 class JobViewSet(viewsets.ModelViewSet):
     serializer_class = JobSerializer
     queryset = Job.objects.all()
+    pagination_class = JobsPagination
 
 
 
