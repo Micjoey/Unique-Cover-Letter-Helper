79dd090a120cf2c3107b1eac7d95e3fe7e95ce98
Macallan Savett <jsavett@gmail.com>
2020-11-24 17:56:03 -0800

    serializers

--- backend/coverLetters/api/serializers.py
+++ backend/coverLetters/api/serializers.py
@@ -4,8 +4,13 @@ from rest_framework import routers, serializers, viewsets
 from coverLetters.models import Job, UserDetail
 
 class JobSerializer(serializers.ModelSerializer):
+    LINK_ERROR = "A link is required."
     class Meta:
         model = Job
         fields = '__all__'
 
-    
+    def validate_job_posting_website(self, value):
+        if value: return value
+        raise serializers.VaidationError(
+            self.LINK_ERROR
+        )
