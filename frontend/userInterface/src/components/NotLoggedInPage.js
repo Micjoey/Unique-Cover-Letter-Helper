8cdb0b9eb05445483812e3de29ce707fb96451fa
Macallan Savett <jsavett@gmail.com>
2020-11-27 14:49:15 -0800

    add a not logged in page

---
+++ frontend/userInterface/src/components/NotLoggedInPage.js
@@ -0,0 +1,7 @@
+const NotLoggedInPage = () => {
+    return (
+        <h1>No user is signed in. Please <a href="/signup"> Sign Up</a> or <a href="/login"> Login</a>.</h1>
+    )
+}
+
+export default NotLoggedInPage
