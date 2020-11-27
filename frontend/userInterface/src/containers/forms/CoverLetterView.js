08d51999529c264b6b1db799ea6cdb3271558385
Macallan Savett <jsavett@gmail.com>
2020-11-27 14:46:16 -0800

    Add in confirmation of user

--- frontend/userInterface/src/containers/forms/CoverLetterView.js
+++ frontend/userInterface/src/containers/forms/CoverLetterView.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { useCallback, } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import * as actions from "../../store/actions/Auth"
+import { useHistory } from 'react-router-dom';
 import { JobForm } from '../../components/cover_letter/CoverLetterForm'
 
 
 const CoverLetterView = () => {
     const paramsJobId = null
+    let history = useHistory()
+    const props = useSelector(state => (
+        {
+            ...state,
+            isAuthenticated: state.token !== null,
+            loading: state.loading,
+            error: state.error
+        }))
+    
+
+
+    const dispatch = useDispatch()
+
     return (
         <div>
-            <JobForm requestType="post" jobID={paramsJobId} buttonTxt="Create Cover Letter"/>
+            {
+                props.isAuthenticated ? 
+                <JobForm requestType="post" jobID={paramsJobId} buttonTxt="Create Cover Letter"/>
+                :
+                <h1>No user is signed in. Please <a href="/signup"> Sign Up</a> or <a href="/login"> Login</a>.</h1>
+            }
         </div>
     )
 }
