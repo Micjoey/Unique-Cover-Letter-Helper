83bfdb22b9eb77cbd208b1a611b36f744cd7f201
Macallan Savett <jsavett@gmail.com>
2020-11-27 14:46:16 -0800

    Add in confirmation of user

--- backend/CoverLetterGenerator/settings.py
+++ backend/CoverLetterGenerator/settings.py
@@ -161,12 +161,11 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-        'rest_framework.permissions.AllowAny'
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        # 'rest_framework.permissions.IsAuthenticated'
+        # 'rest_framework.permissions.AllowAny'
     ],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
-    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    # 'PAGE_SIZE': 20
 }
 
 CORS_ALLOWED_ORIGINS = [
