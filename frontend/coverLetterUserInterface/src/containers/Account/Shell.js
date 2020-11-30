1dcef55cbef668d9ce2cf473c7d54a960f3bb34e
Macallan Savett <jsavett@gmail.com>
2020-11-30 12:46:47 -0800

    revert navBar to proper dropdown

--- frontend/coverLetterUserInterface/src/containers/Account/Shell.js
+++ frontend/coverLetterUserInterface/src/containers/Account/Shell.js
@@ -1,27 +1,52 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
+import {logout} from '../../store/actions/Auth'
 import {Grid, Container, Segment, Header, Menu} from 'semantic-ui-react'
+import { useDispatch } from 'react-redux'
 
-const Shell = props => (
-    <Segment vertical>
-        <Container>
-            <Grid container stackable verticalAlign='middle' divided columns={2}>
-                <Grid.row>
-                    <Grid.Column width={4}>
-                        <Header as="h3">Account</Header>
-                        <Menu vertical fluid>
-                            <Menu.item name="change-email">Change Email</Menu.item>
-                            <Menu.item name="change-email">Change Password</Menu.item>
-                            <Menu.item name="change-email">Change Name</Menu.item>
-                            <Menu.item name="change-email">Logout</Menu.item>
-                        </Menu>
-                    </Grid.Column>
-                    <Grid.Column width={12}>
-                        <h1>{props.children}</h1>
-                    </Grid.Column>
-                </Grid.row>
-            </Grid>
-        </Container>
-    </Segment>
-)
+
+const Shell = props => {
+    const dispatch = useDispatch()
+    const history = useHistory()
+    return (    
+        <Segment vertical>
+            <Container>
+                <Grid container stackable verticalAlign='top' divided columns={2}>
+                    <Grid.Row>
+                        <Grid.Column width={4}>
+                            <Header as="h3">Account</Header>
+                            <Menu vertical fluid>
+                                <Menu.Item 
+                                    onClick={() => history.push("/admin/change-email/")} 
+                                    active={history.location.pathname === "/admin/change-email/"}
+                                    name="change-email">
+                                    Change Email
+                                </Menu.Item>
+                                <Menu.Item onClick={() => history.push("/admin/change-password/")}
+                                    active={history.location.pathname === "/admin/change-password/"}
+                                    name="change-password">
+                                    Change Password
+                                </Menu.Item>
+                                <Menu.Item onClick={() => history.push("/admin/change-account-info/")}
+                                    active={history.location.pathname === "/admin/change-account-info/"}
+                                    name="change-account-info">
+                                    Change Account Info
+                                </Menu.Item>
+                                <Menu.Item onClick={() => dispatch(logout())}
+                                    name="logout">
+                                    Logout
+                                </Menu.Item>
+                            </Menu>
+                        </Grid.Column>
+                        <Grid.Column width={12}>
+                            {props.children}
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </Container>
+        </Segment>
+    )
+}
 
 export default Shell
+
