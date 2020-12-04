663131c4ab1bb13568436e5d9c1a4ab9c4c3b0a8
Macallan Savett <jsavett@gmail.com>
2020-12-03 16:55:34 -0800

    try to host to heroku

--- CoverLetterGenerator/urls.py
+++ CoverLetterGenerator/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.decorators.cache import cache_page
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -19,4 +19,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('rest-auth/facebook/', FacebookLogin.as_view(), name='fb_login')
+    re_path(".*")
 ]
