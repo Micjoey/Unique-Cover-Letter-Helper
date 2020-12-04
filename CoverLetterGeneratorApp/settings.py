1fa4aef14b3f8e7cfa7336e56b68b9a5f4a85be2
Macallan Savett <jsavett@gmail.com>
2020-12-04 00:10:02 -0800

    switch the cors_allowed

--- CoverLetterGeneratorApp/settings.py
+++ CoverLetterGeneratorApp/settings.py
@@ -218,14 +218,20 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:3001",
-#     'http://127.0.0.1:3001',
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3001",
+    'http://127.0.0.1:3001',
+'http://uniquecoverlettergenerator.herokuapp.com',
+'http://www.uniquecoverlettergenerator.com',
+]
 
 # CORS_ORIGIN_ALLOW_ALL = True
-ALLOWED_HOSTS = [
-    'uniquecoverlettergenerator.herokuapp.com', '127.0.0.1', 'heroku.com', 'www.uniquecoverlettergenerator.com']
+# ALLOWED_HOSTS = [
+#     'uniquecoverlettergenerator.herokuapp.com', 
+#     '127.0.0.1', 
+#     'heroku.com', 
+#     'www.uniquecoverlettergenerator.com', 
+#     "localhost:3001", ]
 #                  'http://uniquecoverlettergenerator.com/', 'localhost', 'http://127.0.0.1']
 
 ACCOUNT_CONFIRM_EMAIL_ON_GET = True
