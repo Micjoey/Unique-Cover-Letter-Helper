765783d3a72ae609163bdb130610c223153752da
Macallan Savett <jsavett@gmail.com>
2020-12-03 17:02:35 -0800

    add files

--- CoverLetterGenerator/settings.py
+++ CoverLetterGenerator/settings.py
@@ -77,7 +77,7 @@ ROOT_URLCONF = 'CoverLetterGenerator.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
