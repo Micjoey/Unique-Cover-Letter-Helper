3c71d2eb7759a68f40b96b29770946a07dee3cc5
Macallan Savett <jsavett@gmail.com>
2020-12-04 16:47:49 -0800

    update

--- CoverLetterGeneratorApp/settings.py
+++ CoverLetterGeneratorApp/settings.py
@@ -223,6 +223,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3001',
     'http://uniquecoverlettergenerator.herokuapp.com',
     'http://www.uniquecoverlettergenerator.com',
+    '*'
 ]
 
 # CORS_ORIGIN_ALLOW_ALL = True
