83f2941588a8c90953e6b5c379f84619d119aec1
Macallan Savett <jsavett@gmail.com>
2020-12-06 09:35:51 -0800

    add name ot dorpdown menu

--- src/containers/Banners/Header/NavBar.js
+++ src/containers/Banners/Header/NavBar.js
@@ -5,15 +5,35 @@ import {
   Form, 
   FormControl, 
   Button } from 'react-bootstrap';
-import React, { useCallback, } from 'react'
+import React, { useCallback, useEffect, useState, } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import * as actions from '../../../store/actions/Auth'
 import ErrorBoundary from '../../../store/ErrorBoundary';
+import axios from 'axios'
+import jwtDecode from 'jwt-decode'
 
 
 const Header = () => {
   let history = useHistory()
+  const [user, setUser] = useState({})
+  const accessToken = localStorage.getItem('access_token')
+  
+  const userId = jwtDecode(accessToken).user_id
+  useEffect(() => {
+    axios.defaults.headers = {
+      "Content-type": "application/json",
+      Authorization: `Bearer ${accessToken}`
+    }
+    axios.get(`/api/users/${userId}/`)
+      .then(resp => {
+        setUser(resp.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+
   const props = useSelector(state => (
     {
       ...state, 
@@ -37,7 +57,7 @@ const Header = () => {
 
   return  (
     <div>
-      {props.isAuthenticated ? loggedInNav(props, logout) : loggedOutNav(props, logout)}
+      {props.isAuthenticated ? loggedInNav(props, user, logout) : loggedOutNav(props, logout)}
     </div>
   )
 }
@@ -46,7 +66,7 @@ const Header = () => {
 
 export default Header
 
-const loggedInNav = (props, logout) => (
+const loggedInNav = (props, user, logout) => (
   <Navbar bg="light" expand="lg" className="nav-bar" sticky={true}>
     <Navbar.Brand href="/">Unique Cover Letter Generator</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -56,6 +76,7 @@ const loggedInNav = (props, logout) => (
         <Nav.Link href="/all-jobs">All Jobs</Nav.Link>
         <Nav.Link href="/job/form">Cover Letter Form</Nav.Link>
         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+          <NavDropdown.Header>Welcome {user.preferred_name ? user.preferred_name : user.first_name}</NavDropdown.Header>
           <NavDropdown.Item href="/user-admin/">Account</NavDropdown.Item>
           <NavDropdown.Divider />
           <ErrorBoundary>
