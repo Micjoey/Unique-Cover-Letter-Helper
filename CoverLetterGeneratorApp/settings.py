9ac139fdc35fcbd312bdf0cecc9a9788f6b25bb0
Macallan Savett <jsavett@gmail.com>
2020-12-05 19:24:08 -0800

    fix origins errors

--- CoverLetterGeneratorApp/settings.py
+++ CoverLetterGeneratorApp/settings.py
@@ -27,7 +27,8 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
     'http://uniquecoverlettergenerator.herokuapp.com',
     'http://www.uniquecoverlettergenerator.com',
-    "www.uniquecoverlettergenerator.com",
+    "http://www.uniquecoverlettergenerator.com",
+    "http://uniquecoverlettergenerator.com"
 ]
 
 # CORS_ORIGIN_ALLOW_ALL = True
