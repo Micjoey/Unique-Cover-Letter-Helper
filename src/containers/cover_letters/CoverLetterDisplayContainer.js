d1686be7ce38ef2b527d69c74d206bacfad3c74c
Macallan Savett <jsavett@gmail.com>
2020-12-07 18:15:30 -0800

    put cover letter in a segment

--- src/containers/cover_letters/CoverLetterDisplayContainer.js
+++ src/containers/cover_letters/CoverLetterDisplayContainer.js
@@ -49,25 +49,30 @@ const CoverLetterChoiceContainer = ({job, userId}) => {
 
     if (loaded.isLoaded) {
         return (
-            <div className="cover-letter-display-container">
-                <div>
-                    <Link to="/job/form">Create another cover letter?</Link>
-                </div>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <label>
-                        <p>Template Choices: </p>
-                        <select style={{ color: 'Red' }} name="template_choice" ref={register} style={{ display: 'flex', margin: '0em 1em' }}>
-                            {Object.keys(template_choices).map((key, idx) => (
-                                <option value={key} key={idx} name={template_choices[key]}> {template_choices[key]} </option>
-                                ))}
-                        </select>
-                    </label>
-                    <input className="cover-letter-display-button" type="submit" value="Change Cover Letter" />
-                </form>
-                <div>
-                    {determineCoverLetter(currentCoverLetterChoice, job, user)}
-                </div>
-            </div>
+        <div className="cover-letter-display-container">
+            <Container>
+                <Segment inverted>
+
+                        <div>
+                            <Link to="/job/form">Create another cover letter?</Link>
+                        </div>
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <label>
+                                <p>Template Choices: </p>
+                                <select style={{ color: 'Red' }} name="template_choice" ref={register} style={{ display: 'flex', margin: '0em 1em' }}>
+                                    {Object.keys(template_choices).map((key, idx) => (
+                                        <option value={key} key={idx} name={template_choices[key]}> {template_choices[key]} </option>
+                                        ))}
+                                </select>
+                            </label>
+                            <input className="cover-letter-display-button" type="submit" value="Change Cover Letter" />
+                        </form>
+                        <div>
+                            {determineCoverLetter(currentCoverLetterChoice, job, user)}
+                        </div>
+                </Segment>
+            </Container>
+        </div>
     
         )
     } else {
