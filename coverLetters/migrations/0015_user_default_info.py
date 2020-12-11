f701b9208f1c7bcf3f8e240798746cb4b81d6dda
Macallan Savett <jsavett@gmail.com>
2020-12-11 00:46:06 -0800

    change a migration

--- coverLetters/migrations/0015_user_default_info.py
+++ coverLetters/migrations/0015_user_default_info.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='user',
             name='default_info',
-            field=models.ForeignKey(blank=True, on_delete=django.db.models.deletion.CASCADE, to='coverLetters.defaultinfo'),
+            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='coverLetters.defaultinfo'),
             preserve_default=False,
         ),
     ]
