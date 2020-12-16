84698c39527938b3c5bc1e926f8ff693a2bed060
micjoey <jsavett@gmail.com>
2020-12-16 10:12:48 -0800

    add staff status to dropdown

--- src/containers/Banners/Header/NavBar.js
+++ src/containers/Banners/Header/NavBar.js
@@ -90,7 +90,7 @@ const LoggedInNav = (props, logout, user) => {
               <NavDropdown.Header>Welcome {user.preferred_name ? user.preferred_name : user.first_name}</NavDropdown.Header>
               <NavDropdown.Item href="/user-admin/" className="nav-link">Account</NavDropdown.Item>
               {
-                user.is_superuser ? 
+                user.is_superuser || user.staff_status ? 
                   <NavDropdown.Item href="/admin/" className="nav-link">Admin</NavDropdown.Item> :
                 null
               }
